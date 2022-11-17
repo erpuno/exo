0a36e560198c7833edb674ed5638176d5fd3f894
5HT <maxim@synrc.com>
2022-11-17 17:25:52 +0200

    0.11.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EXO.Mixfile do
   def project() do
     [
       app: :exosculat,
-      version: "0.11.1",
+      version: "0.11.2",
       description: "EXO Exosculat Application",
       package: package(),
       elixir: "~> 1.11",
@@ -26,12 +26,11 @@ defmodule EXO.Mixfile do
 
   def deps() do
     [
-      {:ex_doc, "~> 0.20.2", only: :dev},
+      {:ex_doc, "~> 0.29.0", only: :dev},
       {:cowboy, "~> 2.9.0"},
       {:rocksdb, "~> 1.6.0"},
       {:syn, "2.1.0"},
       {:bpe, "~> 7.11.0"},
-      {:nitro, "~> 7.10.0", override: true},
       {:form, "~> 7.8.0"},
       {:kvs, "~> 9.4.8"},
       {:n2o, "~> 9.11.0"}
